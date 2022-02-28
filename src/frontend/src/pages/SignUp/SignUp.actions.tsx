8e64d656b71411202b3e7b4b73400632a232894d
Ryan Walsh <rcwalsh@gmail.com>
2022-02-28 14:08:37 -0500

    adding console logging to help diagnose signup failure on new Render server (see #17)

--- src/frontend/src/pages/SignUp/SignUp.actions.tsx
+++ src/frontend/src/pages/SignUp/SignUp.actions.tsx
@@ -6,27 +6,29 @@ import { SignUpInputs } from 'shared/user/SignUp'
 export const SIGN_UP_REQUEST = 'SIGN_UP_REQUEST'
 export const SIGN_UP_COMMIT = 'SIGN_UP_COMMIT'
 export const SIGN_UP_ROLLBACK = 'SIGN_UP_ROLLBACK'
-export const signUp = ({ email, password, confirmPassword, username, referral, recaptchaToken }: SignUpInputs) => (
-  dispatch: any,
-) => {
-  dispatch({
-    type: SIGN_UP_REQUEST,
-    payload: {},
-    meta: {
-      offline: {
-        effect: {
-          url: `${process.env.REACT_APP_BACKEND_URL}/user/sign-up`,
-          method: 'POST',
-          json: { email, password, confirmPassword, username, referral, recaptchaToken },
-        },
-        commit: {
-          type: SIGN_UP_COMMIT,
-          meta: {
-            thunks: [showToaster(SUCCESS, `Welcome ${username}!`, 'Happy to see you'), redirect('/')],
+export const signUp =
+  ({ email, password, confirmPassword, username, referral, recaptchaToken }: SignUpInputs) =>
+  (dispatch: any) => {
+    const url = `${process.env.REACT_APP_BACKEND_URL}/user/sign-up`
+    console.log({ url })
+    dispatch({
+      type: SIGN_UP_REQUEST,
+      payload: {},
+      meta: {
+        offline: {
+          effect: {
+            url,
+            method: 'POST',
+            json: { email, password, confirmPassword, username, referral, recaptchaToken },
           },
+          commit: {
+            type: SIGN_UP_COMMIT,
+            meta: {
+              thunks: [showToaster(SUCCESS, `Welcome ${username}!`, 'Happy to see you'), redirect('/')],
+            },
+          },
+          rollback: { type: SIGN_UP_ROLLBACK },
         },
-        rollback: { type: SIGN_UP_ROLLBACK },
       },
-    },
-  })
-}
+    })
+  }
